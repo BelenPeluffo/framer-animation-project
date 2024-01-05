c9ccfb4c8543ea0156e5b2e4be3abc8f3af254a3
Belén Peluffo <belen.peluffo@dirmod.com>
2024-01-05 13:16:20 -0300

    feat (FirstMotionComponent.jsx): deletes initial animations.

--- src/components/FirstMotionComponent.jsx
+++ src/components/FirstMotionComponent.jsx
@@ -4,9 +4,6 @@ import "../assets/styles.css";
 const FirstMotionComponent = ({text}) => {
   return (
     <motion.div
-      initial={{ opacity: 0, scale: 0.5 }}
-      animate={{ opacity: 1, scale: 1 }}
-      transition={{ duration: 1, delay: 0.5 }}
       className="framer-div"
       whileHover={{
         scale: [1, 1.05, 1], // this exact succession of values is what garantees that the motion will be like the object is breathing instead of beating, like a heart.
