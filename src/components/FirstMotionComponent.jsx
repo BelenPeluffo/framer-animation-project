178dede34214486e069c12fa46be0f8f63a37b97
Belén Peluffo <belen.peluffo@dirmod.com>
2024-01-05 10:48:37 -0300

    feat (FirstMotionComponent.jsx): adds "breathing" motion to circle component.

--- src/components/FirstMotionComponent.jsx
+++ src/components/FirstMotionComponent.jsx
@@ -8,6 +8,17 @@ const FirstMotionComponent = () => {
       animate={{ opacity: 1, scale: 1 }}
       transition={{ duration: 1, delay: 0.5 }}
       className="framer-div"
+      whileHover={{
+        scale: [1, 1.05, 1],
+        // duration: 0.25,
+        transition: {
+        //   duration: 0.5,
+          type: "keyframes",
+          repeat: Infinity,
+          delay: 0.25,
+          duration: 1,
+        },
+      }}
     >
       <h1 className="title">This is where it all starts.</h1>
     </motion.div>
