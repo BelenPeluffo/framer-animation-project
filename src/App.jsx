ed0aea0f29fc5607219423bc56bd7625cb307104
Belén Peluffo <belen.peluffo@dirmod.com>
2024-01-05 12:49:48 -0300

    feat (App.jsx): adds navigation.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
-import FirstMotionComponent from "./components/FirstMotionComponent";
 import "./assets/styles.css";
+import { AnimatePresence } from "framer-motion";
+import { Outlet, useNavigate } from "react-router-dom";
 
 function App() {
+  const navigate = useNavigate();
+  const handleClick = () => {
+    navigate('/second-component');
+  }
   return (
     <div className="main-container">
-      <FirstMotionComponent />
+      <AnimatePresence>
+        <Outlet />
+      </AnimatePresence>
+      <button style={{margin: 20}} onClick={handleClick}>Let's go!</button>
     </div>
   );
 }
