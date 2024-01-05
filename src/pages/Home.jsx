64fdb53ae8eccc453f557c476230c3fa955f5144
Belén Peluffo <belen.peluffo@dirmod.com>
2024-01-05 16:08:09 -0300

    feat (Home.jsx): creates default landing page.

---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+const Home = () => {
+  return (
+    <div style={{ height: "100%" }}>
+      <div style={{ height: "60%" }}>Search bar</div>
+      <div style={{ height: "40%" }}>Search results</div>
+    </div>
+  );
+};
+
+export default Home;
