560ac7cb39f11dc824e1a073bbc68669d79ea12a
Belén Peluffo <belenpeluffolupiano@gmail.com>
2024-01-06 12:30:22 -0300

    feat (MockServices.js): updates objects interfaces.

--- src/services/MockServices.js
+++ src/services/MockServices.js
@@ -1,5 +1,5 @@
 export const getItems = () => [
-  { name: "Soyeon" },
-  { name: "Soyeon" },
-  { name: "Soyeon" },
+  { name: "Soyeon", type: "idol", id: 1 },
+  { name: "G-IDLE", type: "group", id: 1 },
+  { name: "Exy", type: "idol", id: 2 },
 ];
