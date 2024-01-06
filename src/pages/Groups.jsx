8ae216f74922c6bb5df7359421463b759a08cd24
Belén Peluffo <belenpeluffolupiano@gmail.com>
2024-01-06 13:22:30 -0300

    feat (Groups.jsx): updates ItemsContainer instance props.

--- src/pages/Groups.jsx
+++ src/pages/Groups.jsx
@@ -4,7 +4,16 @@ import { getItems } from "../services/MockServices";
 
 const Groups = () => {
   const { id } = useParams();
-  return <ItemsContainer title={!id ? "Groups" : ""} items={getItems()} />;
+  // It's gonna be necessary to fetch the groups data so that LOGO can be sent
+  return (
+    <ItemsContainer
+      title={!id ? "Groups" : ""}
+      items={getItems()}
+      logo={
+        id ? "src/assets/images/groupLogos/2560px-(G)I-dle_logo.svg.png" : ""
+      }
+    />
+  );
 };
 
 export default Groups;
