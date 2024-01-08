e49d328ebd2e5de1af2d9b741341068e5068e98e
Belén Peluffo <belen.peluffo@dirmod.com>
2024-01-08 12:21:53 -0300

    refactor (IdentityIcon.jsx): uses react-popper to handle tootlip logic.

--- src/components/IdentityIcon.jsx
+++ src/components/IdentityIcon.jsx
@@ -3,52 +3,24 @@ import { PropTypes } from "prop-types";
 import { useNavigate } from "react-router-dom";
 import "../assets/styles.css";
 import IdentityMiniCard from "./IdentityMiniCard";
-import { createPopper } from "@popperjs/core";
-import { toggleTooltipVisibility } from "../helpers/tooltipActions";
-import { useEffect } from "react";
+import { useState } from "react";
+import { usePopper } from "react-popper";
 
 const IdentityIcon = ({ size, item }) => {
   const navigate = useNavigate();
-  const iconId = `${item.type}-${item.id}`;
-  console.log("iconId?", iconId);
-  const identityCircle = document.querySelector(`#${iconId}`);
-  const tooltip = document.querySelector(`#tooltip-${iconId}`);
-
-  const popperInstance = createPopper(identityCircle, tooltip);
-
-  const showEvents = ["mouseenter", "focus", "hover"];
-  const hideEvents = ["mouseleave", "blur"];
-
-  const handleShow = () => {
-    toggleTooltipVisibility(tooltip, popperInstance, "show");
-  };
-
-  const handleHide = () => {
-    toggleTooltipVisibility(tooltip, popperInstance, "hide");
-  };
-
-  showEvents.forEach((event) => {
-    identityCircle && identityCircle.addEventListener(event, handleShow);
-  });
-
-  hideEvents.forEach((event) => {
-    identityCircle && identityCircle.addEventListener(event, handleHide);
-  });
+  const [identityCircle, setIdentityCircle] = useState(null);
+  const [tooltip, setTooltip] = useState(null);
+  const [showTooltip, setShowTooltip] = useState(false);
+  const { styles, attributes } = usePopper(identityCircle, tooltip);
 
   const handleNavigation = () => {
     navigate(`/${item.type == "group" ? "groups" : "idols"}/${item.id}`);
   };
 
-  useEffect(() => {
-    console.log("identityCircle?", identityCircle);
-    console.log("tooltip?", tooltip);
-    console.log("popperInstance?", popperInstance);
-  }, [identityCircle, tooltip]);
-
   return (
-    <>
+    <div onMouseEnter={() => setShowTooltip(true)}>
       <motion.div
-        id={iconId}
+        ref={setIdentityCircle}
         className={`item-circle ${size}`}
         whileHover={{
           scale: [1, 1.05, 1], // this exact succession of values is what garantees that the motion will be like the object is breathing instead of beating, like a heart.
@@ -64,10 +36,14 @@ const IdentityIcon = ({ size, item }) => {
         {/* Here we could add a Link element instead, so that the URL would be shown on the browser when hovering over */}
         <div className={size === "big" ? "title" : "text"}>{item.name}</div>
       </motion.div>
-      <div id={`tooltip-${iconId}`} className="tooltip" role="tooltip">
+      <div
+        ref={setTooltip}
+        style={(styles.popper, { display: showTooltip })}
+        {...attributes.popper}
+      >
         <IdentityMiniCard item={item} />
       </div>
-    </>
+    </div>
   );
 };
 
