16f0333845b5977383576262bcb0392fdd8a9583
Belén Peluffo <belen.peluffo@dirmod.com>
2024-01-08 09:48:24 -0300

    feat (Idols.jsx): updates props sent to ItemsContainer.

--- src/pages/Idols.jsx
+++ src/pages/Idols.jsx
@@ -1,7 +1,16 @@
+import { useParams } from "react-router-dom";
 import ItemsContainer from "../components/ItemsContainer";
+import { getItems } from "../services/MockServices";
 
 const Idols = () => {
-  return <ItemsContainer items="Members" />;
+  const { id } = useParams();
+  return (
+    <ItemsContainer
+      title={!id ? "Members" : ""}
+      items={getItems()}
+      img={id ? "idol image" : ""}
+    />
+  );
 };
 
 export default Idols;
