edeb4a4ecc5ce3fdede3ee9850fa00e143ed91c2
Belén Peluffo <belen.peluffo@dirmod.com>
2024-01-08 10:53:49 -0300

    feat (IdentityMiniCard.jsx): updates logic.

--- src/components/IdentityMiniCard.jsx
+++ src/components/IdentityMiniCard.jsx
@@ -1,5 +1,15 @@
-const IdentityMiniCard = () => {
-  return <div>IdentityMiniCard</div>;
+import { PropTypes } from "prop-types";
+
+const IdentityMiniCard = ({ item }) => {
+  return <div className="identity-card">{item.name}</div>;
+};
+
+IdentityMiniCard.propTypes = {
+  item: PropTypes.shape({
+    name: PropTypes.string.required,
+    type: PropTypes.string.required,
+    id: PropTypes.number.required,
+  }).required,
 };
 
 export default IdentityMiniCard;
