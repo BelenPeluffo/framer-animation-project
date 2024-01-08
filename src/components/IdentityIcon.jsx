30d96ea3b747de73008beff28b6479b1007a2547
Belén Peluffo <belen.peluffo@dirmod.com>
2024-01-08 16:27:06 -0300

    feat (IdentityIncon.jsx): uses material UI Tooltip component.

--- src/components/IdentityIcon.jsx
+++ src/components/IdentityIcon.jsx
@@ -3,24 +3,18 @@ import { PropTypes } from "prop-types";
 import { useNavigate } from "react-router-dom";
 import "../assets/styles.css";
 import IdentityMiniCard from "./IdentityMiniCard";
-import { useState } from "react";
-import { usePopper } from "react-popper";
+import { Tooltip } from "@mui/material";
 
 const IdentityIcon = ({ size, item }) => {
   const navigate = useNavigate();
-  const [identityCircle, setIdentityCircle] = useState(null);
-  const [tooltip, setTooltip] = useState(null);
-  const [showTooltip, setShowTooltip] = useState(false);
-  const { styles, attributes } = usePopper(identityCircle, tooltip);
 
   const handleNavigation = () => {
     navigate(`/${item.type == "group" ? "groups" : "idols"}/${item.id}`);
   };
 
   return (
-    <div onMouseEnter={() => setShowTooltip(true)}>
+    <Tooltip title={<IdentityMiniCard item={item} />}>
       <motion.div
-        ref={setIdentityCircle}
         className={`item-circle ${size}`}
         whileHover={{
           scale: [1, 1.05, 1], // this exact succession of values is what garantees that the motion will be like the object is breathing instead of beating, like a heart.
@@ -36,14 +30,9 @@ const IdentityIcon = ({ size, item }) => {
         {/* Here we could add a Link element instead, so that the URL would be shown on the browser when hovering over */}
         <div className={size === "big" ? "title" : "text"}>{item.name}</div>
       </motion.div>
-      <div
-        ref={setTooltip}
-        style={(styles.popper, { display: showTooltip })}
-        {...attributes.popper}
-      >
-        <IdentityMiniCard item={item} />
-      </div>
-    </div>
+    </Tooltip>
+
+    // </div>
   );
 };
 
